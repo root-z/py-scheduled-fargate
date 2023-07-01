1f951dc7e2055f9cf75d15c1afc3eac39b9a56ee
Zane Lin <zane.z.lin@gmail.com>
2023-07-01 16:30:25 -0700

    format file

--- infra/infra/infra_stack.py
+++ infra/infra/infra_stack.py
@@ -1,7 +1,5 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
     aws_ec2 as ec2,
     aws_ecs as ecs,
     aws_applicationautoscaling as appscaling,
@@ -10,6 +8,7 @@ from aws_cdk import (
 from aws_cdk.aws_ecr_assets import Platform
 from constructs import Construct
 from pathlib import Path
+
 
 class InfraStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
@@ -21,9 +20,10 @@ class InfraStack(Stack):
             self,
             "ScheduledFargateTask",
             cluster=cluster,
-            scheduled_fargate_task_image_options = ecsp.ScheduledFargateTaskImageOptions(
-                image=ecs.AssetImage(str(Path('..') / 'app'),
-                platform = Platform.LINUX_AMD64),
+            scheduled_fargate_task_image_options=ecsp.ScheduledFargateTaskImageOptions(
+                image=ecs.AssetImage(
+                    str(Path("..") / "app"), platform=Platform.LINUX_AMD64
+                ),
                 memory_limit_mib=512,
             ),
             schedule=appscaling.Schedule.expression("rate(1 day)"),
